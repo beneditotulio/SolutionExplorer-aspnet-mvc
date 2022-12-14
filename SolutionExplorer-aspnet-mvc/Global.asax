955f5e2b1d5bb9cd5fa932aeb28bf8f8b5a87730
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2022-12-14 16:46:23 +0200

    Uploading the project

---
+++ SolutionExplorer-aspnet-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolutionExplorer_aspnet_mvc.MvcApplication" Language="C#" %>
---
+++ SolutionExplorer-aspnet-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SolutionExplorer_aspnet_mvc.Startup))]
+namespace SolutionExplorer_aspnet_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
